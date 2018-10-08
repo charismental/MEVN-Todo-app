584a72ff13ff3aedabbb15440d9760a954893145
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-10-08 12:42:10 -0700

    Styling for Posts table display, setup create method

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
-    <div v-for="(post, index) in posts" :key="index">
-      <h3>{{ post.title }}</h3>
-      <p>{{ post.description }}</p>
+    <div v-if="posts.length > 0" class="table-wrap">
+      <div>
+        <router-link :to="{ name: 'NewPost' }">Add Post</router-link>
+      </div>
+      <table>
+        <tr height="40">
+          <td>Title</td>
+          <td width="550" align="left">Description</td>
+          <td width="100" align="center">Action</td>
+        </tr>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.title }}</td>
+          <td align="left">{{ post.description }}</td>
+          <td align="center">
+            <router-link :to="{ name: 'EditPost', params: { id: post._id } }">Edit</router-link>
+            <a href="#">Delete</a>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div v-else>
+      There are no posts.. Let's add one now <br /><br />
+      <router-link :to="{ name: 'NewPost' }">Add Post</router-link>
     </div>
   </div>
 </template>
@@ -23,12 +43,44 @@ export default {
   methods: {
     async getPosts () {
       const response = await PostsService.fetchPosts()
-      this.posts = response.data
+      this.posts = response.data.posts
     }
   }
 }
 </script>
 
-<style>
-
+<style type="text/css">
+.table-wrap {
+  width: 60%;
+  margin: 0 auto;
+  text-align: center;
+}
+/* table th, table tr {
+  text-align: left;
+} */
+table thead {
+  background: #f2f2f2;
+}
+table tr td {
+  padding: 10px;
+}
+table tr:nth-child(odd) {
+  background: #f2f2f2;
+}
+table tr:nth-child(1) {
+  background: #4d7ef7;
+  color: #fff;
+}
+a {
+  color: #4d7ef7;
+  text-decoration: none;
+}
+a.add_post_link {
+  background: #4d7ef7;
+  color: #fff;
+  padding: 10px 80px;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+}
 </style>
