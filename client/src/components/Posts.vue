f0a7ee98c8fbdf4d60effa7c94dd46105cb555a0
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-10-08 14:05:55 -0700

    Setup delete method

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -16,7 +16,7 @@
           <td align="left">{{ post.description }}</td>
           <td align="center">
             <router-link :to="{ name: 'EditPost', params: { id: post._id } }">Edit</router-link>
-            <a href="#">Delete</a>
+            <a href="#" @click="deletePost(post._id)">Delete</a>
           </td>
         </tr>
       </table>
@@ -44,6 +44,11 @@ export default {
     async getPosts () {
       const response = await PostsService.fetchPosts()
       this.posts = response.data.posts
+    },
+    async deletePost (id) {
+      await PostsService.deletePost(id)
+      this.getPosts()
+      this.$router.push({ name: 'Posts' })
     }
   }
 }
