d38d9b9e36ba371bff4f2ea8562d15e5f50dd1db
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-10-08 11:52:21 -0700

    Install mongoose and connect with mongoDB, set up more endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,48 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const Post = require('../models/post')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/posts')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('Connection Succeeded')
+})
+
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: 'Hellooooo nurse!',
-      description: 'Hey, how\'s it going?'
-    }]
-  )
+  Post.find({}, 'title description', (error, posts) => {
+    if (error) { console.error(error) }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id: -1})
+})
+
+app.post('/posts', (req, res) => {
+  const db = req.db
+  const title = req.body.title
+  const description = req.body.description
+  const new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully'
+    })
+  })
 })
 
 app.listen(process.env.PORT || 8081)
